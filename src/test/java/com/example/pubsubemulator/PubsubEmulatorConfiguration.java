d5692f9d25afab8f0e39ff79581a7ec8c1e19383
PIERRE DUROS <pierre.duros@hotmail.fr>
2025-02-23 17:38:24 +0100

    Works when specifying subscriberTransportChannelProvider and publisherTransportChannelProvider beans without @ConditionalOnProperty

--- src/test/java/com/example/pubsubemulator/PubsubEmulatorConfiguration.java
+++ src/test/java/com/example/pubsubemulator/PubsubEmulatorConfiguration.java
@@ -2,13 +2,41 @@ package com.example.pubsubemulator;
 
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GrpcTransportChannel;
+import com.google.api.gax.rpc.FixedTransportChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
+import com.google.cloud.spring.autoconfigure.pubsub.GcpPubSubProperties;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PreDestroy;
+
 @TestConfiguration(proxyBeanMethods = false)
 class PubsubEmulatorConfiguration {
+    private ManagedChannel channel;
+
     @Bean
     public CredentialsProvider testCredentialsProvider() {
         return new NoCredentialsProvider();
+    }
+
+    @Bean(name = {"subscriberTransportChannelProvider", "publisherTransportChannelProvider"})
+    public TransportChannelProvider transportChannelProvider(
+            GcpPubSubProperties gcpPubSubProperties) {
+        this.channel =
+                ManagedChannelBuilder.forTarget("dns:///" + gcpPubSubProperties.getEmulatorHost())
+                        .usePlaintext()
+                        .build();
+        return FixedTransportChannelProvider.create(GrpcTransportChannel.create(this.channel));
+    }
+
+
+    @PreDestroy
+    public void closeManagedChannel() {
+        if (this.channel != null) {
+            this.channel.shutdown();
+        }
     }
 }
